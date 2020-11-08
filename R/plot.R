d3bb297dc96e4f66bd63f348321cc529e55b7fbe
Lilith Whittles <lilithwhittles@gmail.com>
2020-11-08 11:41:25 +0000

    add plot functions

--- R/plot.R
+++ R/plot.R
@@ -1,30 +1,57 @@
+#' @title plot a flodia
+#' @param f flodia plot function
+#' @param oma single numeric outer margin of plot, default is 1
+#' @importFrom withr with_par
+#' @export
+flodia <- function(f, oma = 0.1) {
 
-#' @title start a flodia plot
-#' @param f flowia plot function
-#' @param x0 left-most x
-#' @param y0 bottom-most y
-#' @param x1 right-most x
-#' @param y1 top-most y
+  z <- flodia_null(f)
+  xlim <- c(z$x0 - oma, z$x1 + oma)
+  ylim <- c(z$y0 - oma, z$y1 + oma)
+
+  withr::with_par(new = list(mar = rep(0, 4), mgp = rep(0, 3), bty = "n",
+                             oma = rep(0, 4)),
+                  code = {
+                    plot(0, 0, type = "n", xlim = xlim, ylim = ylim,
+                         axes = FALSE)
+                    f()
+                  })
+
+  list(x0 = xlim[1], y0 = ylim[1], x1 = xlim[2], y1 = ylim[2])
+}
+
+#' @title save a flodia
+#' @param f flodia plot function
 #' @param filepath desired location of output eg "test.png"
 #' @param width width in pixels of output (height is calculated automatically
 #'  based on plot dimensions)
 #' @param res resolution of plot default = 200 dpi
-#' @param oma outer margin of plot, default is 1
+#' @param oma single numeric outer margin of plot, default is 0.1
 #' @export
 #' @importFrom grDevices png
 #' @importFrom grDevices dev.off
-#' @importFrom graphics par
-#' @importFrom withr with_par
-flodia <- function(f, x0, y0, x1, y1, filepath, width = 1200, res = 200,
-                       oma = c(1, 1, 1, 1)) {
-  height <- round(width * (y1 - y0) / (x1 - x0))
+flodia_png <- function(f, filepath, width = 1200, res = 200, oma = 0.1) {
+
+  # extract co-ordinates of flow diagram
+  z <- flodia(f, oma)
+
+  height <- round(width * (z$y1 - z$y0) / (z$x1 - z$x0))
   png(filepath, width, height, res = res)
-  withr::with_par(new = list(mar = rep(0, 4), mgp = rep(0, 3), bty = "n",
-                             oma = oma),
-                  code = {
-                    plot(0, 0, type = "n", xlim = c(x0, x1),
-                         ylim = c(y0, y1), axes = FALSE)
-                    f()
-                  })
+  flodia(f, oma)
   dev.off()
+}
+
+
+#' @title run a flodia without plotting (e.g to extract co-ordinates)
+#' @param f flodia plot function
+#' @importFrom grDevices pdf
+#' @importFrom graphics plot.new
+flodia_null <- function(f) {
+
+  pdf(NULL)
+  plot.new()
+  z <- f()
+  dev.off()
+
+  z
 }
