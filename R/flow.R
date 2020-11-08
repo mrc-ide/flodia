7c8eb9b592a53035d875398709ae540590576cb2
Lilith Whittles <lilithwhittles@gmail.com>
2020-11-08 11:40:04 +0000

    parameterise consistently

--- R/flow.R
+++ R/flow.R
@@ -1,7 +1,7 @@
 
 #' @title add a flow between two nodes
-#' @param node0 start node
-#' @param node1 end node
+#' @param from start node
+#' @param to end node
 #' @param label label to draw on flow
 #' @param pos a decimal between 0 and 1 giving the position to depart from the
 #' start node, where 0 = bottom / left and 1 = top/right
@@ -20,32 +20,38 @@
 #' @importFrom shape Arrows
 #' @importFrom graphics text
 #' @export
-flow <- function(node0 = NULL, node1 = NULL, label = "",
-                 pos = 0.5, label_pos = 0.5,
-                 label_gap = 0.1, label_col = "black", font = 3,
+flow <- function(from, to, label = NULL,
+                 pos = NULL, label_pos = NULL,
+                 label_gap = NULL, label_col = "black", font = 3,
                  label_cex = 0.8,
                  arr_type = "triangle",
-                 arr_length = 0.2, arr_width = 0.15,
+                 arr_length = 0.2, arr_width = NULL,
                  ...) {
 
-  x0 <- x1 <- calc_pos(node0$x0, node0$x1, pos)
-  y0 <- y1 <- calc_pos(node0$y0, node0$y1, pos)
+  label <- label %||% ""
+  pos <- pos %||% 0.5
+  label_pos <- label_pos %||% 0.5
+  label_gap <- label_gap %||% 0.05
+  arr_width <- arr_width %||% 0.15
 
-  if (node0$y0 > node1$y1) {
-    y0 <- node0$y0
-    y1 <- node1$y1
+  x0 <- x1 <- calc_pos(from$x0, from$x1, pos)
+  y0 <- y1 <- calc_pos(from$y0, from$y1, pos)
+
+  if (from$y0 > to$y1) {
+    y0 <- from$y0
+    y1 <- to$y1
   }
-  if (node0$x0 > node1$x1) {
-    x0 <- node0$x0
-    x1 <- node1$x1
+  if (from$x0 > to$x1) {
+    x0 <- from$x0
+    x1 <- to$x1
   }
-  if (node0$y1 < node1$y0) {
-    y0 <- node0$y1
-    y1 <- node1$y0
+  if (from$y1 < to$y0) {
+    y0 <- from$y1
+    y1 <- to$y0
   }
-  if (node0$x1 < node1$x0) {
-    x0 <- node0$x1
-    x1 <- node1$x0
+  if (from$x1 < to$x0) {
+    x0 <- from$x1
+    x1 <- to$x0
   }
 
   Arrows(x0, y0, x1, y1, arr.type = arr_type, arr.adj = 1,
@@ -53,9 +59,9 @@ flow <- function(node0 = NULL, node1 = NULL, label = "",
 
   if (x0 == x1) {
     x <- x0 + label_gap
-    y <- calc_pos(y0, y1, label_pos)
+    y <- calc_pos(from$y, to$y, label_pos)
   } else if (y0 == y1) {
-    x <- calc_pos(x0, x1, label_pos)
+    x <- calc_pos(from$x, to$x, label_pos)
     y <- y0 + label_gap
   }
 
