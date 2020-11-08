656f77b6ee4eca322d532934bce174f39aefbdd0
Lilith Whittles <lilithwhittles@gmail.com>
2020-11-08 19:25:54 +0000

    add split function

--- R/split.R
+++ R/split.R
@@ -1,19 +1,15 @@
 
-#' @title add fork between three nodes, in direction x
+#' @title add split between three nodes, in direction x
 #' @param from start node
 #' @param to0 the bottom end node
 #' @param to1 the top end node
-#' @param pos a decimal between 0 and 1 giving the position between `from`, and
-#' the closest of `to0` and `to1` at which to split the fork.
 #' @param label_from label to draw on the flow out of `from`
 #' @param label_to0 label to draw on the flow into `to0`
 #' @param label_to1 label to draw on the flow into `to1`
 #' @param pos_from a decimal between 0 and 1 giving the position to start the
 #'  flow out of `from`, where 0 = bottom and 1 = top
-#' @param pos_to0 a decimal between 0 and 1 giving the position to attach the
-#' flow to node `to0`, where 0 = bottom and 1 = top
-#' @param pos_to1 a decimal between 0 and 1 giving the position to attach the
-#' flow to node `to1`, where 0 = bottom and 1 = top
+#' @param pos_to a decimal between 0 and 1 giving the position to attach the
+#' flow to node `to0` and `to1`, where 0 = bottom and 1 = top
 #' @param label_from_pos a decimal between 0 and 1 giving the position along the
 #' flow out of `from` to draw `label_from`, where 0 = start and 1 = end
 #' @param label_to0_pos a decimal between 0 and 1 giving the position along the
@@ -30,53 +26,42 @@
 #' @param ... additional formatting arguments to [flow()]
 #' @return returns the start and end points of the flow
 #' @export
-forkx <-
-  function(from, to0, to1, pos = NULL,
+splitx <-
+  function(from, to0, to1,
            label_from = NULL, label_to0 = NULL, label_to1 = NULL,
-           pos_from = NULL, pos_to0 = NULL, pos_to1 = NULL,
+           pos_from = NULL, pos_to = NULL,
            label_from_pos = NULL, label_to0_pos = NULL, label_to1_pos = NULL,
            label_from_gap = NULL, label_to0_gap = NULL, label_to1_gap = NULL,
            arr_width = NULL, ...) {
 
-    if (abs(to0$x - from$x) < abs(to1$x - from$x)) {
-      nearest <- to0
-    } else {
-      nearest <- to1
-    }
-    split <- node(calc_pos(from$x, nearest$x, pos),
-                  calc_pos(from$y0, from$y1, pos_from),
-                  r = 0)
-
-    flow(from, split, label_from, label_pos = label_from_pos, arr_width = 0,
-         ...)
-    bendy(split, to0, label_to = label_to0, label_to_pos = label_to0_pos,
+    bendx(from, to0, pos_from = pos_from, pos_to = pos_to,
+          label_from = label_from, label_from_pos = label_from_pos,
+          label_from_gap = label_from_gap,
+          label_to = label_to0, label_to_pos = label_to0_pos,
           label_to_gap = label_to0_gap, arr_width = arr_width, ...)
-    bendy(split, to1, label_to = label_to1, label_to_pos = label_to1_pos,
+    bendx(from, to1, pos_from = pos_from, pos_to = pos_to,
+          label_to = label_to1, label_to_pos = label_to1_pos,
           label_to_gap = label_to1_gap, arr_width = arr_width, ...)
 
     list(x0 = min(from$x0, to0$x0, to1$x0),
          y0 = min(from$y0, to0$y0, to1$y0),
          x1 = max(from$x1, to0$x1, to1$x1),
          y1 = max(from$y1, to0$y1, to1$y1),
-         x = split$x, y = split$y)
+         x = from$x, y = from$y)
   }
 
 
-#' @title add fork between three nodes, in direction y
+#' @title add split between three nodes, in direction y
 #' @param from start node
 #' @param to0 the left end node
 #' @param to1 the right end node
-#' @param pos a decimal between 0 and 1 giving the position between `from`, and
-#' the closest of `to0` and `to1` at which to split the fork.
 #' @param label_from label to draw on the flow out of `from`
 #' @param label_to0 label to draw on the flow into `to0`
 #' @param label_to1 label to draw on the flow into `to1`
 #' @param pos_from a decimal between 0 and 1 giving the position to start the
 #'  flow out of `from`, where 0 = left and 1 = right
-#' @param pos_to0 a decimal between 0 and 1 giving the position to attach the
-#' flow to node `to0`, where 0 = left and 1 = right
-#' @param pos_to1 a decimal between 0 and 1 giving the position to attach the
-#' flow to node `to1`, where 0 = left and 1 = right
+#' @param pos_to a decimal between 0 and 1 giving the position to attach the
+#' flow to nodes `to0` and `to1`, where 0 = left and 1 = right
 #' @param label_from_pos a decimal between 0 and 1 giving the position along the
 #' flow out of `from` to draw `label_from`, where 0 = start and 1 = end
 #' @param label_to0_pos a decimal between 0 and 1 giving the position along the
@@ -93,34 +78,26 @@ forkx <-
 #' @param ... additional formatting arguments to [flow()]
 #' @return returns the start and end points of the flow
 #' @export
-forky <-
-  function(from, to0, to1, pos = NULL,
+splity <-
+  function(from, to0, to1,
            label_from = NULL, label_to0 = NULL, label_to1 = NULL,
-           pos_from = NULL, pos_to0 = NULL, pos_to1 = NULL,
+           pos_from = NULL, pos_to = NULL,
            label_from_pos = NULL, label_to0_pos = NULL, label_to1_pos = NULL,
            label_from_gap = NULL, label_to0_gap = NULL, label_to1_gap = NULL,
            arr_width = NULL, ...) {
 
-    if (abs(to0$y - from$y) < abs(to1$y - from$y)) {
-      nearest <- to0
-    } else {
-      nearest <- to1
-    }
-
-    split <- node(calc_pos(from$x0, from$x1, pos_from),
-                  calc_pos(from$y, nearest$y, pos),
-                  r = 0)
-
-    flow(from, split, label_from, label_pos = label_from_pos, arr_width = 0,
-         ...)
-    bendx(split, to0, label_to = label_to0, label_to_pos = label_to0_pos,
+    bendy(from, to0, pos_from = pos_from, pos_to = pos_to,
+          label_from = label_from, label_from_pos = label_from_pos,
+          label_from_gap = label_from_gap,
+          label_to = label_to0, label_to_pos = label_to0_pos,
           label_to_gap = label_to0_gap, arr_width = arr_width, ...)
-    bendx(split, to1, label_to = label_to1, label_to_pos = label_to1_pos,
+    bendy(from, to1, pos_from = pos_from, pos_to = pos_to,
+          label_to = label_to1, label_to_pos = label_to1_pos,
           label_to_gap = label_to1_gap, arr_width = arr_width, ...)
 
     list(x0 = min(from$x0, to0$x0, to1$x0),
          y0 = min(from$y0, to0$y0, to1$y0),
          x1 = max(from$x1, to0$x1, to1$x1),
          y1 = max(from$y1, to0$y1, to1$y1),
-         x = split$x, y = split$y)
+         x = from$x, y = from$y)
   }
