31b9ea1e4f86cb1a853c81f4a1329c2fedaadfdf
Lilith Whittles <lilithwhittles@gmail.com>
2020-12-07 13:53:19 +0000

    return x, y

--- R/group.R
+++ R/group.R
@@ -13,6 +13,8 @@ group <- function(x0, y0, x1, y1, gap = rep(0.1, 4), col = "grey90",
                   border = "black") {
   ret <- list(x0 = x0 - gap[2], y0 = y0 - gap[1],
               x1 = x1 + gap[4], y1 = y1 + gap[3])
+  ret$x <- calc_pos(ret$x0, ret$x1)
+  ret$y <- calc_pos(ret$y0, ret$y1)
   rect(ret$x0, ret$y0, ret$x1, ret$y1, col = col, border = border)
   ret
 }
