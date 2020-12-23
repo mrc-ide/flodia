6a7a2ea62c3dd40b66ac34b78bd35085b456940e
Lilith Whittles <lilithwhittles@gmail.com>
2020-12-23 11:26:25 +0000

    add tests

--- tests/testthat/test-flow.R
+++ tests/testthat/test-flow.R
@@ -1,11 +1,20 @@
 context("flows")
 
-  si <- function() {
+  si <- function(s_x = 0, s_y = 0, i_x = 1, i_y = 0) {
     r <- 0.1
-    s <- node(0, 0, r, label = "S")
-    i <- node(1, 0, r, label = "I")
+    s <- node(s_x, s_y, r, label = "S")
+    i <- node(i_x, i_y, r, label = "I")
     flow(s, i, label = expression(beta))
     list(x0 = s$x0, x1 = i$x1, y0 = s$y0, y1 = s$y1)
   }
 
   vdiffr::expect_doppelganger("SI diagram", fig = function() flodia(si))
+
+  test_that("return error for non-overlapping nodes", {
+    plot.new()
+    expect_error(si(0, 0, 1, 1), "s and i must overlap in the x direction")
+    expect_error(si(0, 0, 1, -1), "s and i must overlap in the x direction")
+    expect_error(si(0, 0, -1, -1), "s and i must overlap in the x direction")
+    expect_error(si(0, 0, -1, 1), "s and i must overlap in the y direction")
+    dev.off()
+  })
