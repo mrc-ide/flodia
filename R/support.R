de50d2b33cee241204fb6f33face78da07f93aee
Lilith Whittles <lilithwhittles@gmail.com>
2020-12-27 15:33:32 +0000

    amend calc_pos so that arguments are invariant to ordering

--- R/support.R
+++ R/support.R
@@ -2,9 +2,9 @@
 #' @title calculates the weighted mean point between two points
 #' @param x0 first point
 #' @param x1 second point
-#' @param pos distance (between 0, 1) from first point, defaults to 0.5
+#' @param pos distance (between 0, 1) from left / bottom point, defaults to 0.5
 #' @export
 calc_pos <- function(x0, x1, pos = NULL) {
   pos <- pos %||% 0.5
-  x0 * (1 - pos) + x1 * pos
+  min(x0, x1) * (1 - pos) + max(x0, x1) * pos
 }
