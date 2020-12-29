efc16925f93e40afc1fd8346ea9fcba53c52935a
Lilith Whittles <lilithwhittles@gmail.com>
2020-12-29 21:12:46 +0000

    allow margins of different sizes at x0, y0, x1 and y1

--- R/plot.R
+++ R/plot.R
@@ -1,12 +1,13 @@
 #' @title plot a flodia
 #' @param f flodia plot function
-#' @param oma single numeric outer margin of plot, default is 1
 #' @param args list of named arguments to `f`
+#' @param oma numeric of length 4 specifying outer margin of plot, at x0, y0,
+#' x1, and y1. Default is 0.1
 #' @importFrom withr with_par
 #' @export
-flodia <- function(f, oma = 0.1, args = list()) {
+flodia <- function(f, oma = rep(0.1, 4), args = list()) {
 
-  z <- flodia_null(f, oma, args)
+  z <- flodia_null(f, args, oma)
 
   withr::with_par(new = list(mar = rep(0, 4), mgp = rep(0, 3), bty = "n",
                              oma = rep(0, 4)),
@@ -23,20 +24,21 @@ flodia <- function(f, oma = 0.1, args = list()) {
 #' @title save a flodia
 #' @param f flodia plot function
 #' @param filepath desired location of output eg "test.png"
+#' @param args list of named arguments to `f`
 #' @param width width in pixels of output (height is calculated automatically
 #'  based on plot dimensions)
 #' @param res resolution of plot default = 200 dpi
-#' @param oma single numeric outer margin of plot, default is 0.1
-#' @param args list of named arguments to `f`
+#' @param oma numeric of length 4 specifying outer margin of plot, at x0, y0,
+#' x1, and y1. Default is 0.1
 #' @param ... other arguments to png()
 #' @export
 #' @importFrom grDevices png
 #' @importFrom grDevices dev.off
-flodia_png <- function(f, filepath, width = 1200, res = 200, oma = 0.1,
+flodia_png <- function(f, filepath, width = 1200, res = 200, oma = rep(0.1, 4),
                        args = list(), ...) {
 
   # extract co-ordinates of flow diagram
-  z <- flodia_null(f, oma, args)
+  z <- flodia_null(f, args, oma)
 
   height <- round(width * (z$y1 - z$y0) / (z$x1 - z$x0))
   png(filepath, width, height, res = res, ...)
@@ -47,22 +49,23 @@ flodia_png <- function(f, filepath, width = 1200, res = 200, oma = 0.1,
 
 #' @title run a flodia without plotting (e.g to extract co-ordinates)
 #' @param f flodia plot function
-#' @param oma single numeric outer margin of plot, default is 0.1
 #' @param args list of named arguments to `f`
+#' @param oma numeric of length 4 specifying outer margin of plot, at x0, y0,
+#' x1, and y1. Default is 0.1
 #' @importFrom grDevices pdf
 #' @importFrom graphics plot.new
 #' @export
-flodia_null <- function(f, oma = 0.1, args = list()) {
+flodia_null <- function(f, args = list(), oma = rep(0.1, 4)) {
 
   pdf(NULL)
   plot.new()
   z <- do.call(f, args)
   dev.off()
 
-  z$x0 <- z$x0 - oma
-  z$x1 <- z$x1 + oma
-  z$y0 <- z$y0 - oma
-  z$y1 <- z$y1 + oma
+  z$x0 <- z$x0 - oma[1]
+  z$y0 <- z$y0 - oma[2]
+  z$x1 <- z$x1 + oma[3]
+  z$y1 <- z$y1 + oma[4]
 
   z
 }
